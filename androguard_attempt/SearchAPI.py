27f8e3a73a89f9e922e9734542965e2b1a4b8265
yjr_win_mechrevo <1355604358@qq.com>
2021-09-11 21:48:18 +0800

    try to finish api part

--- androguard_attempt/SearchAPI.py
+++ androguard_attempt/SearchAPI.py
@@ -12,16 +12,45 @@ def get_androguard_obj(apkfile):
     dx.create_xref()  # 这里需要创建一下交叉引用
     return (a, d, dx)
 
+def output_calling_method(dx):
+    # 要找的api接口：
+    item = dx.find_methods(classname="Landroid/widget/Toast;", methodname="makeText")
+
+    # 输出处理结果
+    fout = open(".\\CallApiMethods.txt", "w",encoding='utf-8')
+    for m in item:
+        print(type(m))
+        print(type(item))
+        print(m)
+        print(item)
+        fout.write("----%s----\n" % (str(m)))
+        cnt = 1  # 输出计数
+        for call in m.get_xref_from():
+            print(call)
+            try:
+                fout.write("[%d]%s\n" % (cnt,str(call)))
+            except:
+                print("write error")
+            cnt += 1
+
+        fout.write('\n\n')
+
+    fout.close()
+
 
 # 要分析的apk路径
-sp = 'D:\\workspace\\Android-immunity\\androguard_attempt\\19300240012.apk'
-# sp = 'D:\\文件\\学习\\课程\\曦源项目\\mwallet\\com.telkom.mwallet_101_apps.evozi.com.apk'
+# sp = 'D:\\workspace\\Android-immunity\\androguard_attempt\\19300240012.apk'
+sp = 'D:\\文件\\学习\\课程\\曦源项目\\mwallet\\com.telkom.mwallet_101_apps.evozi.com.apk'
 
 if __name__ == '__main__':
     # 处理apk
     a, d, dx = get_androguard_obj(sp)
     
-    dx.find_methods(classname="^(?!Landroid).*;$", methodname="<init>", descriptor=r"^\(.+\).*$")
+
+    
+
+    output_calling_method(dx)
+
 
 
 
